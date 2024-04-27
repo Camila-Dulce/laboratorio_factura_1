e01d01ced8340207f82d30daaf3f4d0fb95a8859
David_Santiago_Amaya_Reyes <dsamaya@uniboyaca.edu.co>
2024-04-27 18:27:59 -0500

    coneccion a la base de datos

---
+++ models/Factura.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\models;
+
+class Factura extends Model
+{
+    protected $referencia = " " ;
+    protected $fecha = " ";
+    protected $idCliente = 0 ;
+    protected $estado = " ";
+    protected $descuento = " "; 
+}
+?>
